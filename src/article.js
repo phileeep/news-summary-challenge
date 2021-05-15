de5aa8fafaf6b1080135884d78bac8d360aa17a9
Phil Smithies <philsmithies@gmail.com>
2021-05-15 11:43:49 +0200

    basic build without styling

---
+++ src/article.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class Article {
+  constructor(id, title, text, thumbnail){
+    this.id = id
+    this.title = title
+    this.text = text
+    this.thumbnail = thumbnail
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,63 @@
+let articleDiv = document.getElementById('articlesContainer');
+let articleArrs = []
+let indexId = 0
+
+window.addEventListener('DOMContentLoaded', catchGuardian)
+
+async function catchGuardian(){
+    const response = await fetch('https://content.guardianapis.com/search?show-elements=image&api-key=dc23fc01-7c41-40e7-9bd4-3a13061e59a4&show-fields=all', { method: 'GET' });
+    const data = await response.json()
+    console.log(data)
+    data.response.results.forEach((result) => {
+      const newDiv = document.createElement('div')
+      const headingElement = document.createElement("h1")
+      const photoElement = document.createElement('img')
+      const buttonElement = document.createElement('button')
+      newDiv.appendChild(headingElement)
+      newDiv.appendChild(photoElement)
+      newDiv.appendChild(buttonElement)
+      headingElement.innerHTML = `<a href='#${indexId}'>${result.webTitle}</a>`
+      photoElement.src = `${result.fields.thumbnail}`
+      buttonElement.innerText = "Read Article"
+      buttonElement.setAttribute('href', `${result[indexId]}`)
+      createArticle(`${indexId}`, `${result.webTitle}`, `${result.fields.body}`, `${result.fields.thumbnail}`)
+      articleDiv.appendChild(newDiv)
+
+    })
+};
+
+function createArticle(id, title, text, thumbnail){
+  let article = new Article(id, title, text, thumbnail)
+  articleArrs.push(article)
+  indexId ++
+  return article
+}
+
+makeUrlChangeShowArticle();
+
+function makeUrlChangeShowArticle() {
+  window.addEventListener("hashchange", showArticleForCurrentPage);
+};
+
+function showArticleForCurrentPage(){
+  showArticle(getArticleFromUrl(window.location))
+};
+
+function getArticleFromUrl(location){
+  return location.hash.split("#")[1];
+};
+
+function showArticle(id){
+  const closeButton = document.createElement("a");
+  const articlePhoto = document.createElement('img')
+  closeButton.innerText = "Go Back to Home"
+  closeButton.href = "";
+  articlePhoto.src = `${articleArrs[id].thumbnail}`
+  // articleDiv.innerHTML = articleArrs[id].title
+  articleDiv.innerHTML = `<p class="articleText">${articleArrs[id].text}</p>`
+  articleDiv.appendChild(articlePhoto)
+  articleDiv.appendChild(closeButton)
+}
+
+console.log(articleArrs)
+
